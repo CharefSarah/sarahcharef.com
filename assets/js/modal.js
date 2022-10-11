4affcdb5175bd55d65c1de4eb8beb213952c06a3
CharefSarah <charef.sarah@gmx.fr>
2022-10-11 15:08:21 +0200

    push push

---
+++ assets/js/modal.js
@@ -0,0 +1,73 @@
+
+// Get the modal
+var ebModal = document.getElementById('mySizeChartModal');
+
+
+// Get the button that opens the modal
+var ebBtn = document.getElementById("mySizeChart");
+
+// Get the <span> element that closes the modal
+var ebSpan = document.getElementsByClassName("ebcf_close")[0];
+
+// When the user clicks the button, open the modal 
+ebBtn.onclick = function(){
+    ebModal.style.display = "block";
+    document.querySelector('html').style.overflow = "hidden";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+ebSpan.onclick = function(){
+    ebModal.style.display = "none";
+    document.querySelector('html').style.overflow = "auto";
+}
+
+    
+
+// Get the modal
+var ebModal2 = document.getElementById('mySizeChartModal2');
+
+
+// Get the button that opens the modal
+var ebBtn = document.getElementById("mySizeChart2");
+
+// Get the <span> element that closes the modal
+var ebSpan = document.getElementsByClassName("ebcf_close2")[0];
+
+// When the user clicks the button, open the modal 
+ebBtn.onclick = function(){
+    ebModal2.style.display = "block";
+    document.querySelector('html').style.overflow = "hidden";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+ebSpan.onclick = function(){
+    ebModal2.style.display = "none";
+    document.querySelector('html').style.overflow = "auto";
+}
+
+
+// Get the modal
+var ebModal3 = document.getElementById('mySizeChartModal3');
+
+
+// Get the button that opens the modal
+var ebBtn = document.getElementById("mySizeChart3");
+
+// Get the <span> element that closes the modal
+var ebSpan = document.getElementsByClassName("ebcf_close3")[0];
+
+// When the user clicks the button, open the modal 
+ebBtn.onclick = function(){
+    ebModal3.style.display = "block";
+    document.querySelector('html').style.overflow = "hidden";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+ebSpan.onclick = function(){
+    ebModal3.style.display = "none";
+    document.querySelector('html').style.overflow = "auto";
+}
+
